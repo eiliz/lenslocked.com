571ca651b6cb1c80ded68fc46ef817d5bd831758
Elisabeta Nitura <hello@elisabeta.me>
2019-04-14 16:37:05 +0200

    Added more routes

--- main.go
+++ main.go
@@ -17,9 +17,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact us</h1>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>FAQ</h1>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Not found</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
