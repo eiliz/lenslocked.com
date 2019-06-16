003ef2d55f666391e114f4344f774eb278502bcf
Elisabeta Nitura <hello@elisabeta.me>
2019-06-16 20:50:33 +0200

    Parsing forms with Gorilla schema

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"lenslocked.com/views"
 )
 
@@ -19,16 +20,31 @@ type Users struct {
 	NewView *views.View
 }
 
+// SignupForm maps to new users
+type SignupForm struct {
+	Name     string `schema:"name"`
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // New is used to render the form to signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
 // Create is usee to process the signup form and create a new user account
+// POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 
 	// r.PostForm = map[string][]string
 	fmt.Fprintln(w, r.PostForm["email"])
