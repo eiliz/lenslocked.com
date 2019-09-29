f9829e51ae06bf65abd98e5ede8c85914c2137c3
Elisabeta Nitura <hello@elisabeta.me>
2019-09-29 22:02:06 +0200

    DRYed up the form parsing code

--- controllers/users.go
+++ controllers/users.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"lenslocked.com/views"
 )
 
-// NewUsers is used to ceeate a new Users controller. This function will panic if the templates are not parsed correctly and should only be used during the initial setup
+// NewUsers is used to create a new Users controller. This function will panic if the templates are not parsed correctly and should only be used during the initial setup
 func NewUsers() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
@@ -32,23 +31,12 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
-// Create is usee to process the signup form and create a new user account
+// Create is used to process the signup form and create a new user account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
-
-	// r.PostForm = map[string][]string
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
-	// fmt.Fprintln(w, r.PostFormValue("email"))
-
 }
