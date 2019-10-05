c9f78f2e9d745cff8087a93427c3d98781960c28
Elisabeta Nitura <hello@elisabeta.me>
2019-10-05 21:58:00 +0200

    8.14-Querying-with-GORM

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -35,44 +32,13 @@ func main() {
 
 	defer db.Close()
 
-	if err := db.DB().Ping(); err != nil {
-		panic(err)
-	}
-
 	db.LogMode(true)
 	db.AutoMigrate(&User{})
 
-	name, email, color := getInfo()
-
-	u := User{
-		Name:  name,
-		Email: email,
-		Color: color,
+	var user = User{
+		Color: "navy",
 	}
 
-	if err := db.Create(&u).Error; err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%+v\n", u)
-	// db.DropTableIfExists(&User{})
-}
-
-func getInfo() (name, email, color string) {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println("What's your name?")
-	name, _ = reader.ReadString('\n')
-
-	fmt.Println("What's your email?")
-	email, _ = reader.ReadString('\n')
-
-	fmt.Println("What's your favorite color?")
-	color, _ = reader.ReadString('\n')
-
-	name = strings.TrimSpace(name)
-	email = strings.TrimSpace(email)
-	color = strings.TrimSpace(color)
-
-	return name, email, color
+	db.Where(user).First(&user)
+	fmt.Println(user)
 }
